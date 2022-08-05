0efe4a2ef308855449d70f88c60828747a68073d
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-08-05 14:34:59 -0700

    Update ColorSwitch.tsx

--- src/components/ColorSwitch/ColorSwitch.tsx
+++ src/components/ColorSwitch/ColorSwitch.tsx
@@ -5,38 +5,39 @@ interface ColorSwitchProps {
     colors: Array<string>,
     currentColorIndex?: number, 
     onClick?: () => void, 
-    className?: string, 
     size?: number,
     animDuration?: string,
+    className?: string, 
+    style?: {},
     [x: string]: any,
 }
 
-function ColorSwitch( { currentColorIndex, colors, onClick, className, size=40, animDuration='.4s', ...rest }: ColorSwitchProps ) {
+function ColorSwitch( { currentColorIndex, colors, onClick=()=>{}, size=40, animDuration='.4s', className='', style={}, ...rest }: ColorSwitchProps ) {
 
     const [childCurrentColorIndex, setChildCurrentColorIndex] = useState<number>(currentColorIndex || 0)
     const [selectNum, setSelectNum] = useState<number>(0)
     const colorIndex: number = currentColorIndex || childCurrentColorIndex
 
     const childOnClick: () => void = () => {
-        if (onClick) onClick()
-        
+        onClick()
         setChildCurrentColorIndex((childCurrentColorIndex + 1) % colors.length)
-        setSelectNum(selectNum + 1)
+        setSelectNum((selectNum + 1) % 2)
     }
 
     return <button
-        className='ai-color-switch'
+        className={'ai-color-switch ' + className}
         onClick={childOnClick}
         style={{
             width: size,
             height: size,
             '--ai-color-switch-color': colors[colorIndex],
             '--ai-color-switch-tr-sec': animDuration,
-            '--ai-color-switch-size': `${size*.558}px`
+            '--ai-color-switch-size': `${size*.558}px`,
+            ...style,
         } as React.CSSProperties}
         {...rest}
     >
-        <div aria-hidden={true} className={`ai-color-targ ai-color-targ-${selectNum%2}`}></div>
+        <div aria-hidden={true} className={`ai-color-targ ai-color-targ-${selectNum}`}></div>
     </button>
 }
 
